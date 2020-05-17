2a47879e9fc898f140915dc9e015e3d49fca41ae
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-05-17 17:46:43 +0200

    Added singleton example

---
+++ singleton/singleton_example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace singleton_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.GetInstance();
+            Console.WriteLine(singleton.Info());
+        }
+    }
+}
---
+++ singleton/singleton_example/Singleton.cs
@@ -0,0 +1,34 @@
+﻿namespace singleton_example
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private static object lockObject = new object();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public string Info()
+        {
+            return "Singleton instance";
+        }
+    }
+}
